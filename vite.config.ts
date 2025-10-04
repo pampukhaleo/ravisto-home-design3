f2ddab875940a142b32f400d03ac29bdc4599f88
pampukhaleo <lgpampukha@gmail.com>
2025-10-04 18:28:24 +0300

    gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ravisto-home-design3/',
   server: {
     host: "::",
     port: 8080,
